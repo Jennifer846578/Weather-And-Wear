ad9dc1f4715569095ec2c2a6d8d4f96b539c79f8
isheun <jasonamanda1@gmail.com>
2025-01-03 23:03:39 +0700

    nambah popup homepage

--- resources/views/Components/generate-outfit.blade.php
+++ resources/views/Components/generate-outfit.blade.php
@@ -13,7 +13,18 @@
         <img src="{{ asset('Asset/Wardrobe/Icon Luar/Jeans.png') }}" class="category-icon-image">
         <p>Jeans</p>
     </div>
-    <button class="wear-button">Wear This Outfit</button>
+    <button class="wear-button" onclick="showPopupWear()">Wear This Outfit</button> <!-- Onclick added -->
+</div>
+
+<!-- Popup Wear -->
+<div id="popup-wear" class="popup-wear" style="display: none;">
+    <div class="popup-wear-content">
+        <span class="close-button" onclick="closePopupWear()">&#10005;</span>
+        <div class="check-icon">
+            <svg width="48" height="48" viewBox="0 0 24 24" fill="green"><path d="M9 16.2l-4.2-4.2-1.4 1.4 5.6 5.6 12-12-1.4-1.4z"/></svg>
+        </div>
+        <p>You look so cool!</p>
+    </div>
 </div>
 
 <script>
@@ -28,5 +39,13 @@
 
     function nextItem() {
         alert('Show next item');
+    }
+
+    function showPopupWear() {
+        document.getElementById('popup-wear').style.display = 'flex';
+    }
+
+    function closePopupWear() {
+        document.getElementById('popup-wear').style.display = 'none';
     }
 </script>
