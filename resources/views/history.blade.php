316ab9f5dacbcb89b8d13da4e09fdc7b83c9763d
agathagloriaa <agathagtungky@gmail.com>
2025-03-17 21:18:07 +0700

    update schedule UI

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -83,7 +83,6 @@
 
 
 
-
             {{-- mulai disini
             <div class="dvnweatherhistory">
                 <div class="weatherImages">
@@ -149,9 +148,6 @@
                 </div>
 
             </div> --}}
-
-
-
 
         </div>
     </div>
